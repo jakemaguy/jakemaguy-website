271712d2a02b01045643567b1345c48d4cdbadee
Jake Maguy <jakemaguy@gmail.com>
2019-10-02 13:06:38 -0400

    Created bootstrap for card items on blog page

--- src/app/blog-page/blog-page.component.ts
+++ src/app/blog-page/blog-page.component.ts
@@ -20,27 +20,6 @@ export class BlogPageComponent implements OnInit {
     private modalService: NgbModal) { }
 
   ngOnInit() {
-    this.firebase.getPosts()
-    .subscribe(result => {
-      this.posts = result;
-    });
-  }
-
-  openDialog() {
-    const dialogRef = this.dialog.open(BlogPostCreatorComponent, {
-      height: '1000px',
-      width: '800px',
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result != null) {
-        this.addPost(result);
-      }
-    });
-  }
-
-  addPost(post: BlogPost) {
-    this.posts.push(post);
-    this.firebase.addPost(post);
   }
 }
+
