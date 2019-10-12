b15516d5ca83ff4731dffde9a0968ad30cf84adf
Jake Maguy <jakemaguy@gmail.com>
2019-10-12 06:57:22 -0400

    Added env files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyBQ6bb2yHaLywPOg0QuBq34J5dk3hF7Em8',
+        authDomain: 'jake-maguy',
+        databaseURL: 'https://jake-maguy.firebaseio.com/',
+        projectId: 'jake-maguy',
+        storageBucket: 'gs://jake-maguy.appspot.com',
+        messagingSenderId: '563860953218'
+    }
+};
